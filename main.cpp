05d92c7d4548aa7c3c54427823331bc637e1fb26
Johannes Leppäkorpi <user3610@protonmail.com>
2024-08-16 09:54:00 +0300

    Small tweaks

--- main.cpp
+++ main.cpp
@@ -131,7 +131,7 @@ const char* vertexShaderSource = R"(
 
         // Calculate color based on velocity
         float t = clamp((aVelocity - minVelocity) / (maxVelocity - minVelocity), 0.0, 1.0);
-        particleColor = mix(vec3(0.0, 0.0, 1.0), vec3(1.0, 0.0, 0.0), t);
+        particleColor = mix(vec3(0.1, 0.0, 1.0), vec3(1.0, 0.0, 0.1), t);
     }
 )";
 
